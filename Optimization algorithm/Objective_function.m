d0388a209c7625e7e1c538fcc0d4361ae45da65e
Miguel Angel Bareño Bernal <ing.miguel.bareno@hotmail.com>
2019-07-29 13:31:59 -0300

    Objective fuction

--- Optimization algorithm/Objective_function.m
+++ Optimization algorithm/Objective_function.m
@@ -11,22 +11,24 @@ Pv=x(4); %Pivot
 
 global H;
 global R;
-global Angle_joint;
+global Angle_joint_Max;
 global Resolution_cloud;
 global Penal;
-Angle_joint = evalin('base','Angle_joint'); % Maximun angular value 
+Angle_joint_Max = evalin('base','Angle_joint_Max'); % Maximun angular value 
 H = evalin('base','H'); % Cylinder radius
 R = evalin('base','R'); % Cylinder height
 Resolution_cloud = evalin('base','Resolution_cloud'); 
 Penal = evalin('base','Penal'); 
-R_Draw = 1;%condicion para dibujar el robot
+R_Draw = 0;%condicion para dibujar el robot
+Penalty_fee=1;
 
 %Parameters geometric model of the robot
 
 Robot.Rb=Rb;    %Radius of base [R_b]
-Robot.L=L;    %Length of links [L_e]
+Robot.L=L;      %Length of links [L_e]
 Robot.Rm=Rm;    %Mobile platform radiol [R_m]
 Robot.Pv=Pv;    %Length of rail [L_r]
+Robot.J=10;    %Junta del brazo con la plataforma movil 
 set(gcf,'Color',[1,1,1]);
 
 % Construction of cylindrical points clouds
@@ -52,18 +54,26 @@ for h=(0):Resolution_cloud:T
             q3=q(3);    %Position for actuator 3  
             
 %Check Restrictions 
-if (sum(isnan(q))>0) && (max(q) < Angle_joint)
-    V1=pi*R^2+H;                      % Cylinder volume
-    V2=(pi*((Rb+Pv)^2+Rm^2+((Rb+Pv)*Rm))*z)/3;  % Truncated cone volume 
-    F= (round(V1)+ round(V2))         % Value to minimize 
-end
 
+%minimo angulo 
+Angle_joint_Min=acos(((2*Robot.L)^2-Robot.Pv^2)/(2*Robot.L)^2)
+
+if (sum(isnan(q))>0) && (max(q) < Angle_joint_Max) && (min(q) > Angle_joint_Min)
+    for i=1:3
+        %base del triangulo isoseles 
+        b=sqrt(2*Robot.L^2(1-cos(q(i)));
+        if (b>Robot.Pv)%Posible punto de coolicion
+            Penalty_fee=(Robot.Pv/b)*2;
+        else
+            Penalty_fee= Penalty_fee+(Penal/2);
+        end
+    end
+else
+    Pealty_fee=Penalty_fee+Penal;
+end
+    
+end
 %Draw Robot 
-% if (Pv + Junta == Rb)
-%     V1=pi*R^2+H;                      % Cylinder volume
-%     V2=(pi*((Rb+Pv)^2+Rm^2+((Rb+Pv)*Rm))*z)/3;  % Truncated cone volume 
-%     F= (round(V1)+ round(V2))*100;         % Value to minimize 
-% end
 if (R_Draw == 1)
                     figure(1); clf;
                     a(i)=x;
@@ -76,13 +86,12 @@ if (R_Draw == 1)
                     pause(0.01);
                     i=i+1;
 end
-        end
-    end
+end
 end
 
- V1=pi*R^2+H;                      % Cylinder volume
-    V2=(pi*((Rb+Pv)^2+Rm^2+((Rb+Pv)*Rm))*z)/3;  % Truncated cone volume 
-    F= (round(V1)+ round(V2));         % Value to minimize 
+V1=pi*R^2+H;                      % Cylinder volume
+V2=(pi*((Rb+Pv)^2+Rm^2+((Rb+Pv)*Rm))*z)/3;  % Truncated cone volume 
+F= (round(V1)+ round(V2))+Penalty_fee ;        % Value to minimize 
 end
 
             
